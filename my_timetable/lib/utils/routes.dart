c9d36acab70c425e58d02e7b88e02d1f4526f979
alepistola <pistola.alessandro@virgilio.it>
2020-01-16 17:00:27 +0100

    Changed from flutter to web-page

--- my_timetable/lib/utils/routes.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:my_timetable/screens/Home.dart';
-import 'package:my_timetable/screens/Login.dart';
-
-final routes = {
-  '/login':         (BuildContext context) => new Login(),
-  '/home':         (BuildContext context) => new Home(),
-  '/' :          (BuildContext context) => new Login(),
-};
